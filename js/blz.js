6feef88a7c298d86f888034b2b3cecfc70b4bf67
chanelnumberfive <cococandoit@163.com>
2017-02-03 10:33:49 +0800

    共用blz组件修改
    1 增加备注方便理解和维护
    2 增加requestAnimationFrame方法

--- js/blz.js
+++ js/blz.js
@@ -17,22 +17,41 @@
 }(function($){
 	'use strict';
 	
-	$.blz={};
-	$.blz.emptyFunciton=function(){};
-	$.blz.getDataType=function(data){
-		return Object.prototype.toString.call(data).slice(8,-1).toLowerCase();
-	};
-	$.blz.useragent=/Android/gi.test(navigator.userAgent);
-	$.blz.checkBrowerKernel=function(){
-		var o=document.createElement('div');
-		var a=[['','transition',''],['webkit','Transition','-'],['ms','Transition','-'],['Moz','Transition','-'],['O','Transition','-']];
-		for(var i=a.length-1;i>=0;i--){
-			if(a[i][0]+a[i][1] in o.style){
-				return a[i][2]+a[i][0].toLowerCase()+a[i][2];
-			}else if(i===0){
-				return '';
+	$.blz={
+		
+		// 空函数
+		
+		// 获取数据类型
+		getDataType:function(data){
+			return Object.prototype.toString.call(data).slice(8,-1).toLowerCase();
+		},
+		
+		// 获取用户代理ios 或 android
+		
+		// 检测动画属性transition的支持情况
+		checkTransition:function(){
+			var o=document.createElement('div');
+			var a=[['','transition',''],['webkit','Transition','-'],['ms','Transition','-'],['Moz','Transition','-'],['O','Transition','-']];
+			for(var i=a.length-1;i>=0;i--){
+				if(a[i][0]+a[i][1] in o.style){
+					return a[i][2]+a[i][0].toLowerCase()+a[i][2];
+				}else if(i===0){
+					return false;
+				}
 			}
-		}
+		},
+		
+		// 动画帧函数的兼容处理
+		requestAnimationFrame:function(){
+			if (!window.requestAnimationFrame) {
+				window.requestAnimationFrame=window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame ||window.msRequestAnimationFrame||function(callback) {
+					window.setTimeout(callback, 1000/60);
+				};
+				return window.requestAnimationFrame;
+			}else{
+				return window.requestAnimationFrame;
+			}
+  		}
 	};
 	return $;
 }));
