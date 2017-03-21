70d3801ab2341cceae3cf7a572ba9db3ecf4cba9
chanelnumberfive <cococandoit@163.com>
2017-03-21 10:52:14 +0800

    move组件交互优化

--- js/blz-move.js
+++ js/blz-move.js
@@ -13,17 +13,24 @@
 	/* jshint ignore:end */
 })(function($){
 	'use strict';
-
+	
+	var constent={
+		name:'blzMove',
+		petName:'blz-move',
+		version:'20170321'
+	};
 	var hack=$.blz.checkTransition(),
 		h=window.innerHeight,
 		w=window.innerWidth,
 		config={
 			translateX:0,
-			translateY:0
+			translateY:0,
+			toSide:true,
+			transition:'transform 300ms linear'
 		};
-	
 	// 移动构造函数
 	function Move(elem,option){
+		this.toSideX=(w-elem.offsetWidth)/2;
 		this.x=0;
 		this.y=0;
 		this.shiftX=0;
@@ -38,16 +45,17 @@
 		this.maxShiftX=w-this.clientRect.right+this.prevDx;
 		this.minShiftY=-this.clientRect.top+this.prevDy;
 		this.maxShiftY=h-this.clientRect.bottom+this.prevDy;
+		$.extend(this,config,option||{});
 	}
 	
 	// 移动开始
 	Move.prototype.touchStart=function(e){
 		var event=e.touches[0],
 			$this=$(this),
-			data=$this.data('blz-move');
+			data=$this.data(constent.petName);
 		data.x=event.pageX;
 		data.y=event.pageY;
-		$this.on('touchmove.blzmove',Move.prototype.touchMove);
+		$this.on('touchmove.'+constent.name,Move.prototype.touchMove);
 	};
 	
 	// 移动中
@@ -55,7 +63,7 @@
 		e.preventDefault();
 		var event=e.touches[0],
 			$this=$(this),
-			data=$this.data('blz-move');
+			data=$this.data(constent.petName);
 		data.dx=event.pageX-data.x;
 		data.dy=event.pageY-data.y;
 		data.shiftX=data.dx+data.prevDx<data.minShiftX?data.minShiftX:(data.dx+data.prevDx>data.maxShiftX?data.maxShiftX:data.dx+data.prevDx);
@@ -66,24 +74,46 @@
 	// 移动结束
 	Move.prototype.touchEnd=function(e){
 		var $this=$(this),
-			data=$this.data('blz-move');
+			data=$this.data(constent.petName);
 		data.prevDx=data.shiftX;
 		data.prevDy=data.shiftY;
 		data.dx=data.dy=data.x=data.y=0;
-		$this.off('touchmove.blzmove');
+		$this.off('touchmove.'+constent.name);
+		if(data.toSide){
+			data.toSideFn($this,data);	
+		}
+	};
+	
+	// 贴边
+	Move.prototype.toSideFn=function($elem,data){
+		var cssData={};
+		cssData[hack+'transition']=data.transition;
+		if($elem[0].getBoundingClientRect().left<data.toSideX){
+			cssData[hack+'transform']='translate('+data.minShiftX+'px,'+data.shiftY+'px)';
+			data.prevDx=data.shiftX=data.minShiftX;
+			$elem.css(cssData);
+		}else{
+			cssData[hack+'transform']='translate('+data.maxShiftX+'px,'+data.shiftY+'px)';
+			data.prevDx=data.shiftX=data.maxShiftX;
+			$elem.css(cssData);
+		}
+		$elem.on('transitionend.'+constent.name,function(){
+			$elem.css(hack+'transition','transform 0.0ms linear');
+			$elem.off('transitionend.'+constent.name);
+			console.log('transition');
+		});
 	};
 	
 	// 开启移动
 	$.fn.blzMove=function(option){
-		option=$.extend(config,option||{});
-		this.data('blz-move',new Move(this[0],option));
-		this.on('touchstart.blzmove',Move.prototype.touchStart);
-		this.on('touchend.blzmove',Move.prototype.touchEnd);
+		this.data(constent.petName,new Move(this[0],option));
+		this.on('touchstart.'+constent.name,Move.prototype.touchStart);
+		this.on('touchend.'+constent.name,Move.prototype.touchEnd);
 	};
 	
 	// 关闭移动
 	$.fn.blzOffMove=function(){
-		this.removeData('blz-move');
+		this.removeData(constent.petName);
 		this.off('touchstart.blzmove touchend.blzmove');	
 	};
 	return $;
