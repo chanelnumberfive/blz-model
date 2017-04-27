f78b0c808cd6a31ef30adf0b36d37fe188efb33d
chanelnumberfive <cococandoit@163.com>
2017-04-27 17:02:15 +0800

    修改了transition测试

--- js/blz.js
+++ js/blz.js
@@ -40,8 +40,7 @@
 			for(var i=0;i<a.length;i++){
 				if(a[i][0]+a[i][1] in o.style){
 					return a[i][2]+a[i][0].toLowerCase()+a[i][2];
-				}else if(i===0){
-					return false;
+				}else if(i===4){
 				}
 			}
 		},
